36badbedda5a35d7baa4aee9673683c1fe809b24
Karris  Black <karris.b@gmail.com>
2021-01-06 22:35:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,27 @@
-# require modules here
+require "yaml"
+require "pry"
+def load_library(file_name)
+data = YAML.load_file(file_name)
+hash = {} 
+data.each do |key, value|
+  hash[key] = {:english => value[0], :japanese => value[1]}
+end
+hash
 
-def load_library
-  # code goes here
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_name, emoticon)
+  hash = load_library(file_name)
+  hash.each do 
+  
+  
   # code goes here
 end
 
 def get_english_meaning
   # code goes here
 end
+
+
+
+
